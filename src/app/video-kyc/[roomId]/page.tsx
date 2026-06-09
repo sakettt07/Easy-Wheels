bd38851c87dcd35f664bc02926358db699e67aa6
saket <guptasaket898@gmail.com>
2026-06-09 18:56:56 +0530

    feat: Implement video stream handling and controls in Video KYC page

--- src/app/video-kyc/[roomId]/page.tsx
+++ src/app/video-kyc/[roomId]/page.tsx
@@ -1,16 +1,19 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import Image from 'next/image';
-import { div } from 'motion/react-client';
+import { div, track } from 'motion/react-client';
 
 const page = () => {
     const { userData } = useSelector((state: RootState) => state.user);
     const [joined, setJoined] = useState(false);
-
-    const containerRef = useRef<HTMLDivElement>(null);
+    const previewRef = useRef<HTMLVideoElement | null>(null);
+    const [stream, setStream] = useState<MediaStream | null>(null)
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const [isCameraOn, setIsCameraOn] = useState(true);
+    const [isMicOn, setIsMicOn] = useState(true);
     const startCall = async () => {
         if (!containerRef) {
             return null;
@@ -33,6 +36,35 @@ const page = () => {
             console.log(error)
         }
     }
+
+    useEffect(() => {
+        if (joined) return;
+        let localStream: MediaStream
+        const init = async () => {
+            try {
+                localStream = await navigator.mediaDevices.getUserMedia({
+                    video: true, audio: true
+                })
+                setStream(localStream);
+                if (previewRef.current) {
+                    previewRef.current.srcObject = localStream
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        init()
+    }, [])
+    const toggleCamera = () => {
+        if (!stream) return
+        stream.getVideoTracks().forEach((track) => track.enabled != isCameraOn)
+        setIsCameraOn(!isCameraOn)
+    }
+    const toggleMic = () => {
+        if (!stream) return
+        stream.getAudioTracks().forEach((track) => track.enabled != isMicOn)
+        setIsMicOn(!isMicOn)
+    }
     return (
         <div ref={containerRef} className='min-h-screen bg-black text-white flex flex-col'>
             <div className='px-6 py-4 border-b border-white/10 flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4'>
@@ -46,7 +78,12 @@ const page = () => {
                     <div className="h-full flex items-center justify-center px-4 py-10">
                         <div className='w-ful max-w-6xl grid grid-cols-1 lg:grid-cols-2 gap-12 items-center'>
                             <div className='relative rounded-2xl overflow-hidden border border-white/10 bg-white/5'>
-
+                                <video ref={previewRef} autoPlay muted playsInline />
+                                {!isCameraOn && (
+                                    <div>
+                                        <VideoOff />
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
